a5b4b6186aaf5486d759238abfb44fccd3b91693
MichelKoba <75230242+MichelKoba@users.noreply.github.com>
2020-12-13 16:21:55 -0300

    feat: pag erro

--- app/erro404.php
+++ app/erro404.php
@@ -1,1 +1,38 @@
- 
+ <body style="background-color: white; 
+background-image:url(/assets/img/404bgC.png);">
+
+    <a style=
+    "display: block;
+    position: relative;
+    font-size: 20vw;
+    color: #7d0b23;
+    text-shadow: 10px 10px black;
+    margin-left: 35vw;
+    font-weight: bolder;
+    ">404</a>
+    <a 
+    style="color: white;
+    text-shadow: 10 0 black;
+    font-size: 7vw;
+    font-family: fantasy;
+    display: inherit;
+    margin-left: 15vw;
+    display: inline-block;
+    text-shadow: 10px 10px black;
+    ">Página não encontrada!</a>
+
+    <button style="    
+    width: 45vw;
+    height: 15vh;
+    background-color: #7d0b23;
+    color: white;
+    font-size: 2vw;
+    margin top: 10vh;
+    size: 10vw;
+    margin-left: 28vw;
+    margin-top: 8vh;
+    "
+    >Retornar a tela inicial</button>
+
+
+</body>
