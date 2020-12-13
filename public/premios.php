53144d5e8b985e15f74c9875e6df878b3f245e1b
CaioBrandini <brandinicaio@gmail.com>
2020-12-13 20:27:58 -0300

    feat: premios.php e premios.css

---
+++ public/premios.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+
+	<title>HackoonPão</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/premios.css">
+    <link rel="stylesheet" href="assets/css/userIndex.css">
+  <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+    
+    
+    
+
+<body>
+
+    <header>
+        <img id="logo" src="/assets/img/logoBase.png" width="50px" height="50px" style="margin: 10px">
+        <div class="btns">
+            <img src="/assets/img/mensagem.png">
+            <img src="/assets/img/estrela.png">
+            <img src="/assets/img/user.png">
+        </div>
+    </header>
+
+    <section>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="3200">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="https://source.unsplash.com/1400x500/?premium" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="https://source.unsplash.com/1400x500/?baker" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100"  src="https://source.unsplash.com/1400x500/?butter" alt="Third slide">
+          </div>
+        </div>
+</div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <h1 class="text-dark text-center my-5 ">Prêmios</h1>
+        <div class="row mb-5">
+          <div class="col-sm-12 align-items-center d-flex">
+            <div class="row">
+              <div class="col-sm-4">
+                <div class="card" >
+                  <img class="card-img-top" src="https://source.unsplash.com/250x320/?cake" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Boulos</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="d-flex">
+                      <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                      <h2 style="margin-left: 50px;">R$90</h1>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+                    
+              <div class="col-sm-4">
+                <div class="card" >
+                  <img class="card-img-top" src="https://source.unsplash.com/250x320/?bread" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Pães</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="d-flex">
+                      <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                      <h2 style="margin-left: 50px;">R$35</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          
+              <div class="col-sm-4">
+                <div class="card" >
+                  <img class="card-img-top" src="https://source.unsplash.com/250x320/?Lemon pie" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Torta de limã</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="d-flex">
+                      <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                      <h2 style="margin-left: 50px;">R$100</h1>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row mb-5">
+            <div class="col-sm-4 ">
+              <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/250x320/?lemon" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Limão</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <div class="d-flex">
+                    <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                    <h2 style="margin-left: 50px;">R$22</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+                  
+            <div class="col-sm-4">
+              <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/250x320/?plum" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Ameixa</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <div class="d-flex">
+                    <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                    <h2 style="margin-left: 50px;">R$32</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+        
+            <div class="col-sm-4">
+              <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/250x320/?apple" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Maça</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <div class="d-flex">
+                    <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                    <h2 style="margin-left: 50px;">R$18</h1>
+                  </div>
+                </div>
+              </div>
+
+          </div>
+        </div>
+    </section>
+    </body>
+</html>
