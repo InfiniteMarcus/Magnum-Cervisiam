474c95e438854107d0ea5305b089a5508ee3334d
CaioBrandini <brandinicaio@gmail.com>
2020-12-13 16:52:08 -0300

    fix: index.php, style.css

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -11,10 +11,31 @@
         </div>
 
 
-    <div id="about">
-            <img src="/assets/img/about1.jpeg" alt="Minha Figura" id="img1">	
+    <div class = "about-container">
+        <div class="container-1">
+            <img src="/assets/img/about1.jpeg" alt="Minha Figura"  class="img-fluid img-thumbnail" id="img1">
+            <div class="container-title">
+                <h2>Lorem, ipsum dolor.</h2>
+                <div class="container-border"></div>	
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum eaque dicta voluptatum modi perspiciatis vero in. Ad, minima rem nam sint exercitationem a repellat tenetur, fugiat iure id, dolores ab.</p>
+            </div>
+        </div>
 
-        <h2>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum eaque dicta voluptatum modi perspiciatis vero in. Ad, minima rem nam sint exercitationem a repellat tenetur, fugiat iure id, dolores ab.
-        </h2>
+        <div class="container-1">
+            <div class="container-title">
+                <h2>Lorem, ipsum dolor.</h2>
+                <div class="container-border"></div>	
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum eaque dicta voluptatum modi perspiciatis vero in. Ad, minima rem nam sint exercitationem a repellat tenetur, fugiat iure id, dolores ab.</p>
+            </div>
+            <img src="/assets/img/about2.jpeg" alt="Minha Figura"  class="img-fluid img-thumbnail" id="img1">
+        </div>
+
+        <div class="container-1 mb-0">
+            <img src="/assets/img/about3.jpeg" alt="Minha Figura"  class="img-fluid img-thumbnail" id="img1">
+            <div class="container-title">
+                <h2>Lorem, ipsum dolor.</h2>
+                <div class="container-border"></div>	
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum eaque dicta voluptatum modi perspiciatis vero in. Ad, minima rem nam sint exercitationem a repellat tenetur, fugiat iure id, dolores ab.</p>
+            </div>
+        </div>
     </div>
