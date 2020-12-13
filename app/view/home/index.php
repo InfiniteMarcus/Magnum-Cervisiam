a88f77c65c38076447eb52443ee829949643d22b
MichelKoba <75230242+MichelKoba@users.noreply.github.com>
2020-12-13 05:24:38 -0300

    fix: index feat: usuario

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -1,4 +1,4 @@
-        <header>
+      <header>
             <video autoplay muted loop id="myVideo">
                 <source src="/assets/videos/video.mp4" type="video/mp4">
             </video>
@@ -7,5 +7,14 @@
 
         <div id="title">
             <h1>Ajude a criar sua cerveja perfeita</h1>
-            <button id="button" type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='../home/login'">CADASTRE-SE</button>
+            <button id="button" type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='../home/login'">ENTRE AGORA</button>
         </div>
+
+
+    <div id="about">
+            <img src="/assets/img/about1.jpeg" alt="Minha Figura" id="img1">	
+
+        <h2>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum eaque dicta voluptatum modi perspiciatis vero in. Ad, minima rem nam sint exercitationem a repellat tenetur, fugiat iure id, dolores ab.
+        </h2>
+    </div>
