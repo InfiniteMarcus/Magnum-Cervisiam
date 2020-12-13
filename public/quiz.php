1094b72362a8ac00e87848d2a93510baa3ac99f0
MichelKoba <75230242+MichelKoba@users.noreply.github.com>
2020-12-12 21:07:51 -0300

    feat: Página questionários

---
+++ public/quiz.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Questionário</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/quiz.css">
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+</head>
+
+
+<div class="card">
+    <div class="pergunta">
+        <h1>MEU DEUS CAROLINA, PARA DE SPAMMAR MEU DISCORD</h1>
+    </div>
+
+
+    <div class="form-check form-check-inline">
+    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+    <label class="form-check-label" for="inlineRadio1">1</label>
+
+    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+    <label class="form-check-label" for="inlineRadio2">2</label>
+
+    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
+    <label class="form-check-label" for="inlineRadio3">3</label>
+
+    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4">
+    <label class="form-check-label" for="inlineRadio3">4</label>
+
+    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio5" value="option5">
+    <label class="form-check-label" for="inlineRadio3">5</label>
+    </div>
+
+    <button class="button">
+        MANDAR!
+    </button>
+
+
+</div>
+
+</html>
