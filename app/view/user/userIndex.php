589a1a6ae4ae148a77104fb251034c7870b53dd2
MichelKoba <75230242+MichelKoba@users.noreply.github.com>
2020-12-13 21:49:37 -0300

    Update userIndex.php

--- app/view/user/userIndex.php
+++ app/view/user/userIndex.php
@@ -9,9 +9,27 @@
             <img src="/assets/img/user.png">
         </div>
     </header>
-    <div class="saldo">
-        <h1>Saldo Atual</h1>
-        <h2><?php echo($_GET['saldo']) ?></h2>
+    <div class="card">
+        <div class="card-body">
+            <div class="saldo">
+                <h1>Saldo Atual</h1>
+                <h2><?php echo($_GET['saldo']) ?></h2>
+                <img align="right" id="clocker" src ="/assets/img/clocker.png">
+                <h3 align="right">Pesquisas anteriores</h3>
+                <img align="right" id="local" src ="/assets/img/local.png">
+                <h3 align="right">Locais de retirada</h3>
+
+            </div>
+        </div>
     </div>
+
+    <h1 style="margin-top: 60px; margin-left: 6vw;">Pesquisas ativas</h1>
+    <div class="card" style="margin-top: 10px">
+        <div class="card-body">
+            <h1>Pequisas ativas</h1>
+    </div>
+
+
+
 </body>
 </html>
