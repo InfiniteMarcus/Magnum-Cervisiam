c52fb867df58d386985466587052b013909ecb69
yrmeww <59840433+yrmeww@users.noreply.github.com>
2020-12-13 23:08:55 -0300

    fix: Atualizado o design Página de premios

--- app/view/user/premios.php
+++ app/view/user/premios.php
@@ -11,13 +11,13 @@
       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="3200">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img class="d-block w-100" src="https://source.unsplash.com/1400x500/?premium" alt="First slide">
+            <img class="d-block w-100" src="/assets/img/bg1.png" alt="First slide">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="https://source.unsplash.com/1400x500/?baker" alt="Second slide">
+            <img class="d-block w-100" src="/assets/img/bg2.png" alt="Second slide">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100"  src="https://source.unsplash.com/1400x500/?butter" alt="Third slide">
+            <img class="d-block w-100"  src="/assets/img/bg3.png" alt="Third slide">
           </div>
         </div>
 </div>
@@ -26,18 +26,18 @@
 
     <section class="products">
       <div class="container">
-        <h1 class="text-dark text-center my-5 ">premios</h1>
+        <h1 class="text-dark text-center my-5 ">Recompensas Disponiveis</h1>
         <div class="row mb-5">
           <div class="col-sm-12 align-items-center d-flex">
             <div class="row">
               <div class="col-sm-6">
                 <div class="card" >
-                  <img class="card-img-top" src="https://source.unsplash.com/250x320/?cake" alt="Card image cap">
+                  <img class="card-img-top" src="/assets/img/bicicleta.jpg" alt="Card image cap">
                   <div class="card-body">
-                    <h5 class="card-title"> </h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt sapiente veritatis mollitia!</p>
+                    <h5 class="card-title">Bicicleta</h5>
+                    <p class="card-text">Bicicleta adulta nova, entregue na sua casa.</p>
                     <div class="d-flex">
-                      <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                      <a href="#" class="btn btn-outline-danger">Resgatar</a>
                       <h2 style="margin-left: 50px;">1500 BP</h1>
                     </div>
                     
@@ -47,12 +47,12 @@
                     
               <div class="col-sm-6">
                 <div class="card" >
-                  <img class="card-img-top" src="https://source.unsplash.com/250x320/?bread" alt="Card image cap">
+                  <img class="card-img-top" src="/assets/img/beers.jpg" alt="Card image cap">
                   <div class="card-body">
-                    <h5 class="card-title">Vale alimentação 50R$</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt sapiente veritatis mollitia!</p>
+                    <h5 class="card-title">Vale cerveja 30R$</h5>
+                    <p class="card-text">Adquira um vale no valor de 30 reais pra gastar em cerveja em qualquer loja participando no programa!</p>
                     <div class="d-flex">
-                      <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                      <a href="#" class="btn btn-outline-danger">Resgatar</a>
                       <h2 style="margin-left: 50px;">700 BP</h1>
                     </div>
                   </div>
@@ -66,12 +66,12 @@
         <div class="row mb-5">
             <div class="col-sm-6 ">
               <div class="card" >
-                <img class="card-img-top" src="https://source.unsplash.com/250x320/?lemon" alt="Card image cap">
+                <img class="card-img-top" src="/assets/img/ambev.jpg" alt="Card image cap">
                 <div class="card-body">
-                  <h5 class="card-title">Limão</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt sapiente veritatis mollitia!</p>
+                  <h5 class="card-title">Caneca Personalizada Ambev</h5>
+                  <p class="card-text">A caneca perfeita para levar nas festas e mostrar seu amor pela cerveja de qualidade!</p>
                   <div class="d-flex">
-                    <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                    <a href="#" class="btn btn-outline-danger">Resgatar</a>
                     <h2 style="margin-left: 50px;"> 2500 BP</h1>
                   </div>
                 </div>
@@ -80,12 +80,12 @@
                   
             <div class="col-sm-6">
               <div class="card" >
-                <img class="card-img-top" src="https://source.unsplash.com/250x320/?plum" alt="Card image cap">
+                <img class="card-img-top" src="/assets/img/camisa.png" alt="Card image cap">
                 <div class="card-body">
-                  <h5 class="card-title">Ameixa</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt sapiente veritatis mollitia!</p>
+                  <h5 class="card-title">Camisa Cerveijeiro</h5>
+                  <p class="card-text">Camisa Unissex para os cerveijeiros de plantão, perfeita para aqueles que amam uma boa!</p>
                   <div class="d-flex">
-                    <a href="#" class="btn btn-outline-danger">Ver produto</a>
+                    <a href="#" class="btn btn-outline-danger">Resgatar</a>
                     <h2 style="margin-left: 50px;">600 BP</h1>
                   </div>
                 </div>
